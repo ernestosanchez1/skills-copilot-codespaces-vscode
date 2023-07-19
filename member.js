dd429b380fb3465ebc7f88ea5d858429166f9f1a
Ernesto Sanchez <88904522+ernestosanchez1@users.noreply.github.com>
2023-07-19 01:21:46 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = ['html', 'css', 'js', 'react', 'node', 'python', 'php', 'mysql']
+    var member = {
+        name: 'John',
+        age: 20,
+        skills: skills
+    }
+    return member
+}
